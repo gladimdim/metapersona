b2fcc900811520ea8f98d4095b43bfe7f013758c
Dmytro Gladkyi <gladimdim@gmail.com>
2021-11-04 22:32:33 +0200

    Reading experience data from JSON file.

--- lib/src/experience/experience.dart
+++ lib/src/experience/experience.dart
@@ -34,34 +34,3 @@ class Company {
     return Company(name: input["name"], imageUrl: input["imageUrl"]);
   }
 }
-
-final sap = Company(name: "SAP");
-final globalLogic = Company(name: "GlobalLogic");
-final visiprise = Company(name: "Visiprise");
-final zoomdata = Company(name: "Zoomdata");
-
-List<Experience> myExperience = [
-  Experience(
-      date: DateTime(2018, 12), text: "Senior Software Engineer", company: sap),
-  Experience(
-      date: DateTime(2015, 12),
-      text: "Senior JavaScript Developer",
-      company: zoomdata),
-  Experience(
-      date: DateTime(2015, 6), text: "Development Team Lead", company: sap),
-  Experience(date: DateTime(2013, 5), text: "Software Developer", company: sap),
-  Experience(
-      date: DateTime(2012, 4),
-      text: "Senior Software Test Engineer",
-      company: sap),
-  Experience(
-      date: DateTime(2008, 10), text: "Software Test Engineer", company: sap),
-  Experience(
-      date: DateTime(2007, 12),
-      text: "Software Test Engineer at Visiprise (bought by SAP)",
-      company: visiprise),
-  Experience(
-      date: DateTime(2006, 6),
-      text: "Software Test Engineer",
-      company: globalLogic),
-];
