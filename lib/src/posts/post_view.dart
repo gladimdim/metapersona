f4e61d5490fc8cc8a6beee07d00a2c0a770277b7
Dmytro Gladkyi <gladimdim@gmail.com>
2021-11-08 16:31:03 +0200

    Extracted Markdown and Share libraries to deferred type.

--- lib/src/posts/post_view.dart
+++ lib/src/posts/post_view.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown/flutter_markdown.dart'
+    deferred as flutterMarkdown;
 import 'package:metapersona/src/posts/post.dart';
 import 'package:metapersona/src/utils.dart';
-import 'package:share_plus/share_plus.dart';
+import 'package:share_plus/share_plus.dart' deferred as sharePlus;
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 class PostView extends StatelessWidget {
   static String routeNamePrefix = "/catalog/posts/";
   static String postsPath = "catalog/posts/";
@@ -26,20 +28,25 @@ class PostView extends StatelessWidget {
             appBar: AppBar(
               title: Text(post.title),
               actions: [
-                IconButton(onPressed: () => _copyLink(post), icon: Icon(Icons.copy)),
-                IconButton(onPressed: () => _sharePost(post), icon: Icon(Icons.share)),
+                IconButton(
+                    onPressed: () => _copyLink(post), icon: Icon(Icons.copy)),
+                IconButton(
+                    onPressed: () => _sharePost(post), icon: Icon(Icons.share)),
               ],
             ),
-            body: Markdown(
-              onTapLink: (text, link, title) async {
-                if (link == null) {
-                  return;
-                }
-                await launch(link);
-              },
-              selectable: true,
-              data: "# ${post.title}\n" + post.markdownContent,
-              imageDirectory: getRootUrlPrefix() + postsPath + postId,
+            body: FutureBuilder(
+              future: flutterMarkdown.loadLibrary(),
+              builder: (context, data) => flutterMarkdown.Markdown(
+                onTapLink: (text, link, title) async {
+                  if (link == null) {
+                    return;
+                  }
+                  await launch(link);
+                },
+                selectable: true,
+                data: "# ${post.title}\n" + post.markdownContent,
+                imageDirectory: getRootUrlPrefix() + postsPath + postId,
+              ),
             ),
           );
         } else {
@@ -63,8 +70,8 @@ class PostView extends StatelessWidget {
     Clipboard.setData(ClipboardData(text: Uri.base.toString()));
   }
 
-  void _sharePost(Post post) {
-    Share.share(Uri.base.toString(), subject: post.title);
+  void _sharePost(Post post) async {
+    await sharePlus.loadLibrary();
+    sharePlus.Share.share(Uri.base.toString(), subject: post.title);
   }
-
 }
